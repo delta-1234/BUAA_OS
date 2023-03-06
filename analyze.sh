654bcb1c51bbb7b893642e68e5e9b160e122d997
邓韬 <21373221@buaa.edu.cn>
2023-03-06 20:20:21 +0800

    1

--- analyze.sh
+++ analyze.sh
@@ -3,20 +3,21 @@
 if [ $# -eq 1 ];
 then
     # Your code here. (1/4)
+    cat $1 | grep -E "WARN | ERROR"  > bug.txt
 
 else
     case $2 in
     "--latest")
         # Your code here. (2/4)
-
+	tail -5 $1	
     ;;
     "--find")
         # Your code here. (3/4)
-
+	cat $1 | grep -E "$3" > $3.txt
     ;;
     "--diff")
         # Your code here. (4/4)
-
+	diff -q $1 $3
     ;;
     esac
 fi

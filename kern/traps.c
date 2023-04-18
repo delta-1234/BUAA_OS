a960da163537bdba2296a2cecea2b3d978702507
邓韬 <21373221@buaa.edu.cn>
2023-04-18 16:02:06 +0800

    2

--- kern/traps.c
+++ kern/traps.c
@@ -13,12 +13,13 @@ extern void handle_ov(void);
 void (*exception_handlers[32])(void) = {
     [0 ... 31] = handle_reserved,
     [0] = handle_int,
+	[12] = handle_ov,
     [2 ... 3] = handle_tlb,
 #if !defined(LAB) || LAB >= 4
     [1] = handle_mod,
     [8] = handle_sys,
 #endif
-    [12] = handle_ov,
+    
 };
 
 /* Overview:
@@ -30,13 +31,15 @@ void do_ov(struct Trapframe *tf) {
 	u_long epc = tf->cp0_epc;
 	struct Env *e = curenv;
 	Pte *pte = NULL;
-	page_lookup(e->env_pgdir, epc, &pte);
-	u_int zl = *((u_int *)KADDR(PTE_ADDR(*pte)));
-	u_int addi = (2) << 28;
+	struct Page * page = page_lookup(e->env_pgdir, epc, &pte);
+	u_int zl = *((u_int *)((KADDR(PTE_ADDR(*pte))) | (epc & 0xfff)));
+	u_int addi = (15) << 28;
 	u_int d, s, t, imm;
 	e->env_ov_cnt++;
 	s = (zl << 6) >> 27;
 	t = (zl << 11) >> 27;
+	// printk("%x", zl);
+	// panic("%d", zl & addi);
 	if ((zl & addi) != 0) {
 		imm = (zl << 16) >> 16;
 		tf->regs[t] = tf->regs[s] / 2 + imm / 2;
@@ -44,10 +47,10 @@ void do_ov(struct Trapframe *tf) {
 		printk("addi ov handled\n");
 	} else {
 		if ((zl & 15) == 0) {
-			*((u_int *)KADDR(PTE_ADDR(*pte))) = zl | 1;
+			*((u_int *)((KADDR(PTE_ADDR(*pte))) | (epc & 0xfff))) = zl | 1;
 			printk("add ov handled\n");
 		} else if ((zl & 15) == 2) {
-			*((u_int *)KADDR(PTE_ADDR(*pte))) = zl | 1;
+			*((u_int *)((KADDR(PTE_ADDR(*pte))) | (epc & 0xfff))) = zl | 1;
 			printk("sub ov handled\n");
 		}
 	}
